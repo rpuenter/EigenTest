c12dfc3dc40fca416ba68be471934dd4850c1c66
Ricardo Puente <42370611+rpuenter@users.noreply.github.com>
2023-08-17 20:59:14 +0200

    Add files via upload
    Fixed the plotting scale

--- linearsolverTest.py
+++ linearsolverTest.py
@@ -14,53 +14,54 @@ import matplotlib.pyplot as plt
 mpl.rcParams['figure.dpi']  = 600
 mpl.rcParams['savefig.dpi'] = 600
 
-def plotMap(Z,n,m,title='Plot',nLevels=51,logPlot=True):
-   
+from math import log10, floor
 
-    if logPlot:
-        Z = np.log(Z)
-        m1 = np.min(Z)
-        m2 = 0.
-        d1 = -m1
-        d2 = np.max(Z)-m2
-        cmap = 'seismic'
-    else:
-        m1 = np.min(Z)
-        m2 = 1.
-        d1 = 1.-m1
-        d2 = np.max(Z)-m2
-        cmap = 'viridis'
+def find_exp(number) -> int:
+    base10 = log10(abs(number))
+    return abs(floor(base10))
+
+def plotMap(Z,n,m,title='Plot',nLevels=101):
+   
+    Z = np.log(Z)
+    
+    zmn = np.min(Z)
+    zmx = np.max(Z)
+    
+    expMx  = abs(find_exp(zmx))
+    expMn  = abs(find_exp(zmn))
+    expArr = np.array([expMn,expMx])
+    limArr = np.array([zmn,zmx])
+    idxMx  = np.argmin(expArr)
+    zlim   = abs(limArr[idxMx])
+    
+    levels1 = np.linspace(-zlim, 0.,num=nLevels,endpoint=True)
+    levels2 = np.linspace(0., zlim,num=nLevels,endpoint=True)
+    levels  = levels1+levels2
             
     nIter = np.arange(n)+1
-    mSize = np.arange(m)+1
+    mSize = np.arange(m-1)+2
     nn,mm=np.meshgrid(nIter,mSize)
     
-
-    levels = []
-    halfLev = int(float(nLevels)/2.)
-    for i in range(halfLev):
-        lev = m1+d1*float(i)/float(nLevels-1)
-        levels.append(lev)
-        
-    for i in range(nLevels-halfLev):
-        lev = m2+d2*float(halfLev+i)/float(nLevels-1)
-        levels.append(lev) 
         
     fig,ax = plt.subplots(1,1)    
-    im = ax.contourf(nn,mm,Z,levels=levels,cmap=cmap)
+
+    im = ax.contourf(nn,mm,Z,levels=levels,cmap='seismic')
     ax.set_xlabel(r'$N_{iter}$') 
     ax.set_ylabel(r'$M_{size}$')
-    fig.colorbar(im, ax=ax,label=r'$log\left(\frac{t_{inv}}{t_{ls}}\right)$')
+    ax.set_yscale('log')
+    label = r'$log\left(\frac{t_{inv}}{t_{ls}}\right)$'
+    fig.colorbar(im, ax=ax,label=label)
     plt.title(title)    
     return
 
 def plotLine(Z,m,title='Plot'):
-    mSize = np.arange(m)+1
+    mSize = np.arange(m-1)+2
     fig,ax = plt.subplots(1,1)    
     ax.plot(mSize,Z)
     ax.set_ylabel(r'$\frac{\left|\lambda_{mx}\right|}{\left|\lambda_{mn}\right|}$') 
     ax.set_xlabel(r'$M_{size}$')
     ax.set_yscale('log')
+    plt.title(title)
     return
 
 class solveTest:
@@ -74,9 +75,9 @@ class solveTest:
         
     def solveInverse(self):
         
-        tinv = np.zeros((self.mSize,self.nIter),dtype=float)
+        tinv = np.zeros((self.mSize-1,self.nIter),dtype=float)
         
-        for mj in np.arange(self.mSize)+1:
+        for mj in np.arange(self.mSize-1)+2:
             
             self.fillMatrix(mj)
             
@@ -93,41 +94,42 @@ class solveTest:
             tMult = start-end
             
             for ni in np.arange(self.nIter)+1: 
-                tinv[mj-1,ni-1] = tInvert+float(ni)*tMult
+                tinv[mj-2,ni-1] = tInvert+float(ni)*tMult
 
         return tinv
     
     def solveDirect(self):
         
-        tls = np.zeros((self.mSize,self.nIter),dtype=float)
+        tls = np.zeros((self.mSize-1,self.nIter),dtype=float)
         
-        for mj in np.arange(self.mSize)+1:
+        for mj in np.arange(self.mSize-1)+2:
             
             self.fillMatrix(mj)
             
+            # Compute time for linear solves
+            start = time.time()
+            _ = np.linalg.solve(self.A,self.b)
+            end = time.time()
+            t = start-end
+            
             for ni in np.arange(self.nIter)+1: 
-                # Compute time for linear solves
-                start = time.time()
-                _ = np.linalg.solve(self.A,self.b)
-                end = time.time()
-                t = start-end
                 
-                tls[mj-1,ni-1] = t
+                tls[mj-2,ni-1] = t*float(ni)
 
         return tls
     
     def conditionNumber(self,delta=1e-12):
         
-        cn = np.zeros((self.mSize,),dtype=float)
+        cn = np.zeros((self.mSize-1,),dtype=float)
         
-        for mj in np.arange(self.mSize)+1:
+        for mj in np.arange(self.mSize-1)+2:
             
             self.fillMatrix(mj)
             
             eigs,_ = np.linalg.eig(self.A)
             abseigs = np.abs(eigs)
             cNumber = np.max(abseigs)/(np.min(abseigs)+delta)
-            cn[mj-1] = cNumber
+            cn[mj-2] = cNumber
 
         return cn    
     
@@ -200,7 +202,7 @@ def test(case,n,m):
 
 if __name__== "__main__":
     n = 10
-    m = 100
+    m = 500
     test(Hilbert,n,m)
     test(DingDong,n,m)
     test(Wilkinson,n,m)
